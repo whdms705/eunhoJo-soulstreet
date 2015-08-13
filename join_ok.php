623330c54017b6e7b345f39b1c2d6bb565202fe5
whdms705 <whdms705@nate.com>
2015-08-13 21:20:18 +0900

    로그인 성공시

---
+++ join_ok.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Soul Street</title>
+		<meta charset='UTF-8' />
+		<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
+		<meta name='format-detection' content='telephone=no' />
+		<link rel="stylesheet" type="text/css" href="./css/login.css" />
+	</head>
+
+	<body>
+		<div class='container'>
+			<div class="wrapper">
+				<div class="image"></div>
+				<div class="ok">
+					<h2 class="title">안녕하세요. <?echo ""; //session?>님!</h2>
+					<h4 class="subtitle">소울스트릿을 통해 버스커들은 자신들의 공연을 홍보할 수 있고,<br>
+					일반 사용자들은 버스킹 공연 정보를 받아볼 수 있어요!</h4>
+					<a href="index.php"><div class="login_btn">소울스트릿 시작하기
+					</div></a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
