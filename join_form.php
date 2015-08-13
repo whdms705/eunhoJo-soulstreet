0b25ab2c426fcbe95787e7550289b583f385791f
whdms705 <whdms705@nate.com>
2015-08-13 21:20:01 +0900

    가입 폼

---
+++ join_form.php
@@ -0,0 +1,38 @@
+																				<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Soul Street</title>
+		<meta charset='UTF-8' />
+		<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' />
+		<meta name='format-detection' content='telephone=no' />
+		<link rel="stylesheet" type="text/css" href="./css/login.css" />
+	</head>
+
+	<body>
+		<div class='container'>
+			<div class="wrapper">
+				<div class="image"></div>
+				<div class="join">
+					<div class="input">
+						<input class="box" type = "email" placeholder="이메일" name="email">
+					</div>
+					<div class="input">
+						<input class="box" type = "text" placeholder="닉네임" name="name">
+					</div>
+					<div class="input">
+						<input class="box" type = "password" placeholder="비밀번호" name="password">
+					</div>
+					<div class="input">
+						<input class="box" type = "password" placeholder="비밀번호" name="password_check">
+					</div>
+					<a href="#"><div class="login_btn">등록하기
+					</div></a>
+					<div class="option">
+						<a href="login_form.php"><div class="back">돌아가기</div></a>
+					</div>
+				</div>
+			</div>
+			<div class="policy">계정을 만들면 소울스트릿의 서비스 약관 및 개인정보 보호정책에<br>동의하는 것으로 간주합니다.</div>
+		</div>
+	</body>
+</html>
